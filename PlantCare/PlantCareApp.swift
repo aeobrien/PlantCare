9d929788e9404b3faef3cc912c6f36a0820c26ae
Aidan O'Brien <aidan.e.obrien@gmail.com>
2025-06-07 12:56:12 +0100

    Initial Commit

---
+++ PlantCare/PlantCareApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlantCareApp.swift
+//  PlantCare
+//
+//  Created by Aidan O'Brien on 07/06/2025.
+//
+
+import SwiftUI
+
+@main
+struct PlantCareApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

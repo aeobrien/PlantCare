9b0772c813f19bef39d33dbde2d60b1aa722fa9e
Commit Guardian <guardian@localhost>
2026-09-05 02:08:30 +0100

    Commit Guardian snapshot aidans-laptop 2026-09-05 PlantCare-69a3b8b2cb4a

---
+++ snapshot/PlantCareTests/PlantCareTests.swift
@@ -0,0 +1,17 @@
+//
+//  PlantCareTests.swift
+//  PlantCareTests
+//
+//  Created by Aidan O'Brien on 07/06/2025.
+//
+
+import Testing
+@testable import PlantCare
+
+struct PlantCareTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
